5aed379db43c49265f454ca5e02e9569108c7cf6
mayur shinde <kssmsslss330@gmail.com>
2024-03-20 22:35:34 +0530

    ""

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
-import { hostname } from 'os';
-import {nextui}  from "@nextui-org/react";
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
@@ -15,18 +14,9 @@ const nextConfig = {
             
         ]
         
-    },
-    content: [
-        // ...
-        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-      ],
-      theme: {
-        extend: {},
-      },
-      darkMode: "class",
-      plugins: [nextui()],
+    }
 
 
 
 };
- 
+ export default nextConfig;
